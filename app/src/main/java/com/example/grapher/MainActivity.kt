d26b822a6508a190a661fee79c49f9869b9962d1
tines <tinesvalen@gmail.com>
2021-02-24 14:36:15 +0100

    Added button with intent to GraphActivity

--- app/src/main/java/com/example/grapher/MainActivity.kt
+++ app/src/main/java/com/example/grapher/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.grapher
 
-import model.DefaultVertex;
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import model.DefaultVertex
+
 
 /** AppCompatActivity replaces Activity in this library */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button = findViewById<Button>(R.id.bruhOst)
 
+        button.setOnClickListener{
+            startActivity(Intent(this, GraphActivity::class.java))
+        }
         DefaultVertex.resetCounter()
     }
+
+
+
 }
