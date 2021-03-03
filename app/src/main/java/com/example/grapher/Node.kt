86689af48a7005e4cbcbbb6e92579054f79029b9
tines <tinesvalen@gmail.com>
2021-03-03 13:55:20 +0100

    Changed node to draw XML instead of using Paint

--- app/src/main/java/com/example/grapher/Node.kt
+++ app/src/main/java/com/example/grapher/Node.kt
@@ -1,32 +1,22 @@
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 
 import android.view.View
+import android.view.ViewGroup
+import com.example.grapher.R
 
 
 class Node(context: Context?,x: Float, y: Float, w: Int, h: Int) : View(context) {
-    var paint : Paint
     var w: Int = w
     var h: Int = h
     var posX = x
     var posY = y
 
     init {
-        paint = Paint()
-        paint.setColor(Color.CYAN)
-        paint.setStyle(Paint.Style.FILL)
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        canvas?.drawCircle(posX, posY, w.toFloat(), paint)
-    }
-
-    fun changeColor(){
-        paint.setColor(Color.MAGENTA)
+        setX(posX-w/2)
+        setY(posY-h/2)
+        setBackgroundResource(R.drawable.circle)
+        var params = ViewGroup.LayoutParams(w, h)
+        layoutParams = params
     }
 }
