958e974ea598ba854678a23c64cf27875361c47a
Kristoffer <kris.aesoey@gmail.com>
2021-03-06 15:17:01 +0100

    Removed unnecessary imports

--- app/src/main/java/com/example/grapher/GraphActivity.kt
+++ app/src/main/java/com/example/grapher/GraphActivity.kt
@@ -1,10 +1,6 @@
 package com.example.grapher
 
-import Node
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import model.DefaultVertex
 
