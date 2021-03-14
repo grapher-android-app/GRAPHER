280841ec32ac4bede4a8b8b5478cdb4ca91705f1
Katrine <gix009@uib.no>
2021-03-14 15:25:01 +0100

    Added a popup menu function

--- app/src/main/java/com/example/grapher/GraphActivity.kt
+++ app/src/main/java/com/example/grapher/GraphActivity.kt
@@ -2,10 +2,12 @@ package com.example.grapher
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.CompoundButton
-import android.widget.Switch
-import android.widget.TextView
-import android.widget.Toast
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.*
+import android.widget.Toast.makeText
 import androidx.appcompat.app.AppCompatActivity
 import model.Node
 
@@ -28,7 +30,26 @@ class GraphActivity : AppCompatActivity() {
             }
         }
 
+        val hamburgerButton = findViewById<Button>(R.id.hamburger)
+        hamburgerButton?.setOnClickListener{
+            showPopUp(hamburgerButton)
+        }
+
     }
 
+    //Maybe put this into a class later, but for now, its a menu :D
+    private fun showPopUp(view: View){
+        val popMenu = PopupMenu(this, view)
+        popMenu.inflate(R.menu.algorithm_menu)
 
+        popMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.test1 -> Toast.makeText(this, "lol", Toast.LENGTH_SHORT).show()
+                R.id.test2 -> Toast.makeText(this, "loli", Toast.LENGTH_SHORT).show();
+                else -> Toast.makeText(this, "lollol", Toast.LENGTH_SHORT).show();
+            }
+            true
+        }
+        popMenu.show()
+    }
 }
