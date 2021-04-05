81cd56e9a8283ff05ebe9dc33f9354b10b707ffe
Katrine <gix009@uib.no>
2021-04-05 14:06:40 +0200

    added centerInspector kotlin version

---
+++ app/src/main/java/algorithms/CenterInspector.kt
@@ -0,0 +1,66 @@
+package algorithms
+
+import org.jgrapht.graph.DefaultUndirectedGraph;
+import org.jgrapht.alg.connectivity.ConnectivityInspector
+import org.jgrapht.alg.shortestpath.FloydWarshallShortestPaths
+import kotlin.collections.HashMap
+import kotlin.math.max
+
+class CenterInspector {
+
+    companion object{
+       fun <V,E> getCenter(graph: DefaultUndirectedGraph<V, E>):  V {
+           var center: V? = null
+           var minDistance: Int = graph.vertexSet().size
+           val paths = FloydWarshallShortestPaths<V, E>(graph)
+
+
+           val vertexToAntiSize : Map<V, Int> = calculateComponents(graph)
+
+           for(v: V in graph.vertexSet()){
+               var currentDistance: Int = 0
+
+               for(u: V in graph.vertexSet()){
+                   if(u != v){
+
+                       var distance: Double = paths.getPath(v, u).length.toDouble()
+
+                       if (distance == Double.POSITIVE_INFINITY){
+                           distance = vertexToAntiSize[v]?.toDouble()!!
+
+                       }
+
+                       currentDistance = max(currentDistance.toDouble(), distance).toInt()
+                   }
+               }
+               if(currentDistance < minDistance){
+                   minDistance = currentDistance
+                   center = v
+               }
+           }
+            return center!!
+       }
+
+        private fun <V,E> calculateComponents(graph: DefaultUndirectedGraph<V, E>): Map<V, Int> {
+            val vertexToAntiSize = HashMap<V, Int>()
+            val ci = ConnectivityInspector<V, E>(graph)
+
+            val ccs = ci.connectedSets()
+            var largestComponent: Int = 0
+
+            for(cc in ccs){
+                largestComponent = max(largestComponent, cc.size)
+            }
+
+            for(i in 0..ccs.size){
+                val componentSize : Int = ccs[i].size
+                for (v: V in ccs[i]){
+                    vertexToAntiSize[v] = (largestComponent - componentSize)
+                }
+            }
+            return vertexToAntiSize
+        }
+    }
+
+
+}
