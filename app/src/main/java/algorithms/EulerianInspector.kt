cb0539e467dfb799be22af7f7c460ef28c99094f
Katrine <gix009@uib.no>
2021-04-07 10:19:50 +0200

    Implemented EulerianInspector algorithm

---
+++ app/src/main/java/algorithms/EulerianInspector.kt
@@ -0,0 +1,36 @@
+package algorithms
+
+import org.jgrapht.graph.SimpleGraph
+
+class EulerianInspector {
+
+
+    companion object{
+        fun <V, E> isEulerian(g: SimpleGraph<V, E>?): Boolean{
+            if(g==null){
+                println(g!!) //this will throw nullpointerexception if null
+            }
+
+            for(v: V in g!!.vertexSet()){
+                if(g.edgesOf(v).size % 2 != 0){
+                    return false
+                }
+            }
+            return true
+        }
+
+        fun <V, E> getOddDegreeVertices(g: SimpleGraph<V, E>?): HashSet<V> {
+            if(g==null){
+                println(g!!) //this will throw nullpointerexception if null
+            }
+            var odds  = HashSet<V>(g.vertexSet().size)
+
+            for(v: V in g!!.vertexSet()){
+                if(g.edgesOf(v).size % 2 != 0){
+                    odds.add(v)
+                }
+            }
+            return odds
+        }
+    }
+}
